3d35db66d8ecb4cc112ae090683bb41c4615c955
Markus Smehyl <13markuso@gmail.com>
2026-04-15 09:08:39 +0200

    original version of code

--- LinDesk-Linux-Desktop-Environment-Simulator/obj/Debug/net9.0-windows/App.g.cs
+++ LinDesk-Linux-Desktop-Environment-Simulator/obj/Debug/net9.0-windows/App.g.cs
@@ -45,7 +45,7 @@ namespace LinDesk_Linux_Desktop_Environment_Simulator {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "10.0.5.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.11.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace LinDesk_Linux_Desktop_Environment_Simulator {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "10.0.5.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.11.0")]
         public static void Main() {
             LinDesk_Linux_Desktop_Environment_Simulator.App app = new LinDesk_Linux_Desktop_Environment_Simulator.App();
             app.InitializeComponent();
